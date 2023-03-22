24658beceb5983191080de27406e74690117a834
vennela jangam <vennela.jangam@nagarro.com>
2023-03-22 19:38:53 +0530

    first commit

---
+++ q1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string directoryPath = @"C:\Users\UserName\Documents"; // Replace with the path of the directory you want to analyze
+
+        // Get all the files in the directory
+        var files = Directory.GetFiles(directoryPath);
+
+        // Return the number of text files in the directory (*.txt)
+        var textFiles = files.Where(f => f.EndsWith(".txt"));
+        Console.WriteLine($"Number of text files: {textFiles.Count()}");
+
+        // Return the number of files per extension type
+        var filesByExtension = files.GroupBy(f => Path.GetExtension(f));
+        foreach (var group in filesByExtension)
+        {
+            Console.WriteLine($"Number of {group.Key} files: {group.Count()}");
+        }
+
+        // Return the top 5 largest files, along with their file size (use anonymous types)
+        var largestFiles = files.Select(f => new { Name = Path.GetFileName(f), Size = new FileInfo(f).Length })
+                               .OrderByDescending(f => f.Size)
+                               .Take(5);
+        Console.WriteLine("Top 5 largest files:");
+        foreach (var file in largestFiles)
+        {
+            Console.WriteLine($"{file.Name} ({file.Size} bytes)");
+        }
+
+        // Return the file with maximum length
+        var longestFile = files.OrderByDescending(f => new FileInfo(f).Length).FirstOrDefault();
+        Console.WriteLine($"File with maximum length: {longestFile}");
+    }
+}
+
---
+++ q2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int attempts = 0;
+
+        while (attempts < 5)
+        {
+            Console.WriteLine("Enter any number from 1-5 :-");
+            int input = 0;
+
+            try
+            {
+                input = Convert.ToInt32(Console.ReadLine());
+                if (!IsValidInput(input))
+                {
+                    throw new InvalidInputException("Invalid input! Please enter a number from 1-5.");
+                }
+
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("Enter even number");
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter odd number");
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter a prime number");
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter a negative number");
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter zero");
+                        break;
+                }
+
+              
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input! Please enter a number.");
+            }
+            catch (InvalidInputException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            attempts++;
+        }
+
+        Console.WriteLine("You have played this game for 5 times.");
+        Console.ReadLine();
+    }
+
+    static bool IsValidInput(int input)
+    {
+        return input >= 1 && input <= 5;
+    }
+}
+
+class InvalidInputException : Exception
+{
+    public InvalidInputException(string message) : base(message)
+    {
+    }
+}
